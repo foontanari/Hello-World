c20b1ecc338ce35b2c9babb3d4fadd1048202b49
foontanari <mcfontanari@gmail.com>
2026-03-09 11:21:50 -0300

    Add files via upload

--- python verdadeiro agr.py
+++ python verdadeiro agr.py
@@ -1,1 +1,3 @@
-print("hello world")
+print("Hello World!")
+print("Estou aprendendo python agora.")
+print("Esse é o primeiro projeto que estou criando no curso de Engenharia de Computação na UFC.")
